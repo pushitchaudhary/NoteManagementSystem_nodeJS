1c0042c27b075c8279d4a19ee688ca0607fa8f3c
pushitchaudhary <pushitcoc@gmail.com>
2023-09-18 23:48:12 +0545

    parapass1

--- app.js
+++ app.js
@@ -61,8 +61,8 @@ app.get('/',(req,res)=>{
 })
 
 // home page ma jaan ko lagi
-app.get('/:id', async (req, res) => {
-    const paraid = req.params.id;
+app.get('/', async (req, res) => {
+    // const paraid = req.params.id;
 
     // Assuming 'paraid' is the user's ID, retrieve data based on this ID from your database
     // For example, fetch the user's blog posts
@@ -75,7 +75,7 @@ app.get('/:id', async (req, res) => {
     // You can fetch other data as needed
 
     // Then render the 'blog' view with the data
-    res.render('blog', { userBlogPosts });
+    res.render('blog');
 })
 
 
