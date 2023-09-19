d4f90ef8eb79bb93649dfa0dd11974743cbdb185
pushitchaudhary <pushitcoc@gmail.com>
2023-09-19 21:23:00 +0545

    final

--- model/index.js
+++ model/index.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   operatorsAliases: false,
-  // port: 7100,
+  port: 7100,
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
