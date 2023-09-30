3cc9512b7e059219061acb7e5356e5fa30270e11
pushitchaudhary <pushitcoc@gmail.com>
2023-09-30 08:56:39 +0545

    image validation

--- middleware/multerConfig.js
+++ middleware/multerConfig.js
@@ -2,6 +2,17 @@ const multer = require("multer");
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
+
+    // supported image file type
+    const supportedImageFormat = ['image/jpeg','image/jpg','image/png']
+    // image type verfiy garn ko lagi
+    const fileSize = file.size;
+    console.log("file size is", fileSize)
+    if(!supportedImageFormat.includes(file.mimetype)){
+        cb(new Error("Image Type not supported"))
+        return
+    }
+
     cb(null, "./uploads/");
   },
   filename: function (req, file, cb) {
