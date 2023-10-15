ae7a16849e35129b9716b3f423165d6364837b9b
pushitchaudhary <pushitcoc@gmail.com>
2023-10-15 18:01:10 +0545

    error handling and flash - updateBlog post route

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -10,7 +10,7 @@ const catchError = require('../services/catchError');
 const upload = multer({ storage: storage });
 
 router.route('/createBlog').get(catchError(isLoggedIn), catchError(RenderCreateBlog)).post(catchError(isLoggedIn),upload.single('image'), catchError(PostCreateBlog));
-router.route('/updateBlog/:id').get(catchError(isLoggedIn) ,catchError(RenderEditBlog)).post(isLoggedIn, upload.single('image'), PostEditBlog)
+router.route('/updateBlog/:id').get(catchError(isLoggedIn) ,catchError(RenderEditBlog)).post(catchError(isLoggedIn), upload.single('image'), catchError(PostEditBlog))
 router.route('/home').get(isLoggedIn, renderHomePage)   // home page ma jaan ko lagi
 router.route('/singleBlog/:postId').get(isLoggedIn, RenderSingleBlog)   // single blog show garna ko lagi
 router.route('/deleteBlog/:postNum').get(isLoggedIn ,RenderBlogDelete) // post delete garna ko lagi
