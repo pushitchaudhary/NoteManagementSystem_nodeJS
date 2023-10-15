7f5918b8a4e17552acd3c14eccebf28c92180ff7
pushitchaudhary <pushitcoc@gmail.com>
2023-10-15 16:52:18 +0545

    error handling and flash - otpCode route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -15,7 +15,7 @@ router.route('accountDeletedAlert').get(isLoggedIn, RenderAccountDelete)    // a
 router.route('/forgetPassword').get(ForgetPassword).post(PostForgetPassword)
 router.route('/resetYourPassword/:id').get(ResetYourPassword).post(PostResetYourPassword)
 router.route('/identify_account/:id').get(identify_account).post(PostIdentify_account) 
-router.route('/otpCode/:email').get(RenderOtpCode).post(PostRenderOtpCode)
+router.route('/otpCode/:email').get(catchError(RenderOtpCode)).post(catchError(PostRenderOtpCode))
 router.route('/newPassword').get(catchError(RenderNewPassword)).post(catchError(PostNewPassword))
 
 router.route('/register').get(catchError(RenderRegisterPage)).post(catchError(PostUserRegisters))   // Register page ma jaan / Register garn
