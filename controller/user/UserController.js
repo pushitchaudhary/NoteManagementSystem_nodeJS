c3cde62f2c69ca7b5f8963a34159f4d322403bbe
pushitchaudhary <pushitcoc@gmail.com>
2023-10-15 07:32:48 +0545

    update

--- controller/user/UserController.js
+++ controller/user/UserController.js
@@ -422,7 +422,7 @@ exports.PostNewPassword = async (req,res)=>{
             res.send("Server Error")
         }
     }else{
-        console.log("Password and confirm ")
+        console.log("Password and confirm s")
         res.send("Password and confirm password don't match")
     }
 
