9dd3f9a6f810ce07b324c3fce4d8c94402c1b97d
pushitchaudhary <pushitcoc@gmail.com>
2023-10-15 17:53:39 +0545

    error handling and flash - debug createblog route -image

--- middleware/multerConfig.js
+++ middleware/multerConfig.js
@@ -2,17 +2,19 @@ const multer = require("multer");
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-
-    // supported image file type
-    const supportedImageFormat = ['image/jpeg','image/jpg','image/png']
-    // image type verfiy garn ko lagi
-    const fileSize = file.size;
-    if(!supportedImageFormat.includes(file.mimetype)){
-        cb(new Error("Image Type not supported"))
-        return
-    }
-
+    try {
+      // supported image file type
+      const supportedImageFormat = ['image/jpeg','image/jpg','image/png']
+      // image type verfiy garn ko lagi
+      const fileSize = file.size;
+      if(!supportedImageFormat.includes(file.mimetype)){
+          cb(new Error("Image Type not supported"))
+          return
+      }
     cb(null, "./uploads/");
+    } catch (error) {
+      cb(res.send('Error'))
+    }
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -9,8 +9,8 @@ const { multer, storage } = require("../middleware/multerConfig");
 const catchError = require('../services/catchError');
 const upload = multer({ storage: storage });
 
-router.route('/createBlog').get(catchError(isLoggedIn), catchError(RenderCreateBlog)).post(catchError(isLoggedIn), catchError(upload.single('image')), catchError(PostCreateBlog))
-router.route('/updateBlog/:id').get(catchError(isLoggedIn),catchError(RenderEditBlog)).post(catchError(isLoggedIn), catchError(upload.single('image')), catchError(PostEditBlog))
+router.route('/createBlog').get(isLoggedIn,RenderCreateBlog).post(catchError(isLoggedIn),upload.single('image'), catchError(PostCreateBlog));
+router.route('/updateBlog/:id').get(isLoggedIn ,RenderEditBlog).post(isLoggedIn, upload.single('image'), PostEditBlog)
 router.route('/home').get(isLoggedIn, renderHomePage)   // home page ma jaan ko lagi
 router.route('/singleBlog/:postId').get(isLoggedIn, RenderSingleBlog)   // single blog show garna ko lagi
 router.route('/deleteBlog/:postNum').get(isLoggedIn ,RenderBlogDelete) // post delete garna ko lagi
